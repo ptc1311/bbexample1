948945601ef59ca53ea4d32c7c1977f28b66f597
TC Chen <tc.chen@insyde.com>
2023-11-24 14:01:26 +0800

    add sdbusplus subprojects

--- bbexample.cpp
+++ bbexample.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdarg.h>
+#include <algorithm>
+#include <functional>
 
 #include "bbfunction1.hpp"
 #include "bbfunction2.hpp"
@@ -11,21 +14,51 @@
 #include "bbfunction9.hpp"
 #include "bbfunction10.hpp"
 
+void myprintf(const char* fmt, ...)
+{
+    char buf[128];
+
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(buf, fmt, ap);
+    va_end(ap);
+
+    printf("%s", buf);
+}
+
+void func1(int int1, char char1) {
+
+}
+
+int g_int = 0;
+void changePtr(int* &pInt) {
+    pInt = &g_int;
+}
 int main(int argc, char *argv[])
 {
   std::cout << "Hello from main!" << std::endl;
   std::cout << "Hello from main!" << std::endl;
 
-  function1();
-  function2();
-  function3();
-  function4();
-  function5();
-  function6();
-  function7();
-  function8();
-  function9();
-  function10();
+	myprintf("%d\n", 1);
+    myprintf("%d %d\n", 1, 2);
+    myprintf("%d %d %s\n", 1, 2, "Hello World");
+
+
+    int localInt = 1;
+    int* localPInt = &localInt;
+    changePtr(localPInt);
+    printf("%d\n", *localPInt);
+
+    function1();
+    /*function2();
+    function3();
+    function4();
+    function5();
+    function6();
+    function7();
+    function8();
+    function9();
+    function10();*/
   
   return 0;
 }
--- inc/bbfunction1.hpp
+++ inc/bbfunction1.hpp
@@ -1,13 +1,104 @@
 #include <nlohmann/json.hpp>
 
+
+bool mycompare(int a, int b) {
+    return a > b; // 降序排列
+}
+
+struct myclass {
+    bool operator() (int a, int b) { return a > b; }  // 降序排列
+} myobject;
+
 inline void function1() {
-    std::cout << "Function 1" << std::endl;
+    //nlohman json: 
+    std::cout << "========json dump========" << std::endl;
     nlohmann::json jsonExample = {
         {"name", "John"},
         {"age", 30},
         {"city", "New York"}
     };
   
-    std::cout << jsonExample.dump(2) << std::endl;
+    std::cout << jsonExample.dump(2) << std::endl << std::endl;
+
+    std::cout << "========sort========" << std::endl;
+    //sort
+    int arr[] = {4, 5, 8, 3, 7, 1, 2, 6, 10, 9};
+    //std::sort(arr, arr+10);
+    
+    //below is better for human read
+    //std::sort(std::begin(arr), std::end(arr));
+
+    //decreasing
+    std::sort(std::begin(arr), std::end(arr), std::greater<int>());
+
+    std::cout << "sort array by default (increasing):" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "========sort vecotr by function (decreasing)========" << std::endl;
+    int arr2[] = {5, 4, 1, 7, 3, 8, 9, 10, 6, 2};
+    std::vector<int> v(arr2, arr2+10);
+    std::sort(v.begin(), v.end(), mycompare);
+    for (int i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "========sort vecotr by lambda function (increasing):========" << std::endl;
+    std::vector<int> vlambda = {5, 4, 1, 7, 3, 8, 9, 10, 6, 2};
+    std::sort(vlambda.begin(), vlambda.end(), [](int a, int b){
+        return a < b; // 升序排列
+    });
+    for (int i : vlambda) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "========sort vecotr by object (decreasing):========" << std::endl;
+    std::vector<int> vobj = {5, 4, 1, 7, 3, 8, 9, 10, 6, 2};
+    std::sort(vobj.begin(), vobj.end(), myobject);
+    for (int i : vobj) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::string s11 = "eacdb";
+    std::sort(s11.begin(), s11.end(), std::less<char>());
+    std::cout << "========sort string (increasing):========" << std::endl;
+    for (char c : s11) {
+        std::cout << c << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "========range-based for loop (string):========" << std::endl;
+    std::string str = "hello world";
+    for (auto &c : str) {
+        std::cout << c << '\n';
+    }
+    std::cout << std::endl << std::endl;
+
+    std::string s1;
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+    s1 += "abcdefghij";
+    s1 += "abcdefghij";
+    s1 += "abcdefghij";
+    s1 += "abcdefghij";
+    s1 += "abcdefghij";
+    std::cout << "size=" << s1.size() << ", capacity=" << s1.capacity() << "\n";
+
+    std::cout << std::endl << std::endl;
+
 }
 
